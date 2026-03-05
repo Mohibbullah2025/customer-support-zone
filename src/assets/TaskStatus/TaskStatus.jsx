a976db2b17caca15a378fbae746327c586ce0ffc
Al Helal Mohibbullah <alhelalmohibbullah@gmail.com>
2026-03-05 22:36:23 +0600

    complete task status

--- src/assets/TaskStatus/TaskStatus.jsx
+++ src/assets/TaskStatus/TaskStatus.jsx
@@ -1,9 +1,22 @@
 
 const TaskStatus = ({selectedIssue}) => {
   console.log(selectedIssue)
-  return (
-    <div className="mt-20">
-      <h1 className="text-xl font-medium">Task Status</h1>
+  
+  if(!selectedIssue){
+    return(
+      <div>
+        <h2 className="mt-20 text-xl mb-10">Task Status</h2>
+        <p>Select a ticket to add to Task Status</p>     
+        </div>
+    )
+  }
+  return(
+    <div>
+      <h2 className="mt-20 text-xl mb-10">Task Status</h2>
+      <div>
+        <h3 className="font-semibold">{selectedIssue.title}</h3>
+        <button className="btn btn-success w-full mt-4">Complete</button>
+      </div>
     </div>
   )
 };
